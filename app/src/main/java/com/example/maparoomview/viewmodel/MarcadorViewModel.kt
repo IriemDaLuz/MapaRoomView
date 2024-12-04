6431c6d5ffef9b10aaf8bdc09d00c7eb997a62bd
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-04 09:29:36 +0000

    creamos viewmodel

---
+++ app/src/main/java/com/example/maparoomview/viewmodel/MarcadorViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.maparoomview.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MarcadorViewModel(application: Application) : AndroidViewModel(application) {
+
+}
