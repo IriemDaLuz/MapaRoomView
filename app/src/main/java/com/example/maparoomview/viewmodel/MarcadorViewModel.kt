0589d64989c309650691cd47a8a93e5664a58073
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-04 09:30:46 +0000

    creamos funcion para obtener los marcadores

--- app/src/main/java/com/example/maparoomview/viewmodel/MarcadorViewModel.kt
+++ app/src/main/java/com/example/maparoomview/viewmodel/MarcadorViewModel.kt
@@ -4,12 +4,19 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.maparoomview.database.MarcadorDatabase
 import com.example.maparoomview.models.Marcador
+import kotlinx.coroutines.launch
 
 class MarcadorViewModel(application: Application) : AndroidViewModel(application) {
     private val marcadorDao = MarcadorDatabase.getDatabase(application).marcadorDao()
     private val _marcadores = MutableLiveData<List<Marcador>>()
     val marcadores: LiveData<List<Marcador>> get() = _marcadores
 
+    fun obtenerMarcadores() {
+        viewModelScope.launch {
+            _marcadores.value = marcadorDao.obtenerTodosLosMarcadores()
+        }
+    }
 }
