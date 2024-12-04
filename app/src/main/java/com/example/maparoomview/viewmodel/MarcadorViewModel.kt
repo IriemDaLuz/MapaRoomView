6e4d6fc57d9beac0f3d4639c56d8f2ca1299ae80
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-04 09:30:01 +0000

    creamos variables viewmodel

--- app/src/main/java/com/example/maparoomview/viewmodel/MarcadorViewModel.kt
+++ app/src/main/java/com/example/maparoomview/viewmodel/MarcadorViewModel.kt
@@ -2,7 +2,14 @@ package com.example.maparoomview.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.maparoomview.database.MarcadorDatabase
+import com.example.maparoomview.models.Marcador
 
 class MarcadorViewModel(application: Application) : AndroidViewModel(application) {
+    private val marcadorDao = MarcadorDatabase.getDatabase(application).marcadorDao()
+    private val _marcadores = MutableLiveData<List<Marcador>>()
+    val marcadores: LiveData<List<Marcador>> get() = _marcadores
 
 }
