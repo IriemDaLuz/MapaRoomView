14768b977b6ea5614cd94e614ad8fdcdf943f28a
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-10 13:15:20 +0000

    creo dataclasy tabla lugares

---
+++ app/src/main/java/com/example/maparoomview/data/Lugar.kt
@@ -0,0 +1,25 @@
+package com.example.maparoomview
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.ForeignKey
+
+@Entity(
+    tableName = "lugares",
+    foreignKeys = [
+        ForeignKey(
+            entity = TipoLugar::class,
+            parentColumns = ["id"],
+            childColumns = ["IdTipoLugar"],
+        )
+    ]
+)
+
+data class Lugar(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val latitude: String,
+    val longitude: String,
+    val IdTipoLugar: Int
+)
