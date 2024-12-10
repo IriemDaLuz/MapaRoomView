3fc26c0b252d5c6205c4cb8ea4168c9eb6335fd0
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-10 13:17:23 +0000

    archivo mainAppMap

---
+++ app/src/main/java/com/example/maparoomview/view/MainMapApp.kt
@@ -0,0 +1,40 @@
+package com.example.maparoomview
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import com.utsman.osmandcompose.*
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
+import org.osmdroid.tileprovider.tilesource.XYTileSource
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.util.MapTileIndex
+
+val GoogleSat: OnlineTileSourceBase = object : XYTileSource(
+    "Google-Sat",
+    0, 19, 256, ".png", arrayOf(
+        "https://mt0.google.com",
+        "https://mt1.google.com",
+        "https://mt2.google.com",
+        "https://mt3.google.com",
+    )
+) {
+    override fun getTileURLString(pTileIndex: Long): String {
+        return baseUrl + "/vt/lyrs=s&x=" + MapTileIndex.getX(pTileIndex) + "&y=" + MapTileIndex.getY(
+            pTileIndex
+        ) + "&z=" + MapTileIndex.getZoom(pTileIndex)
+    }
+}
