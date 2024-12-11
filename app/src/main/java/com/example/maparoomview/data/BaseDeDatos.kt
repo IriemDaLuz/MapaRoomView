6869302a4b4fcf5686e103f0e68102646eb8113d
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-11 08:40:51 +0000

    creamos otra version de la basededatos para actualizar los marcadores

--- app/src/main/java/com/example/maparoomview/data/BaseDeDatos.kt
+++ app/src/main/java/com/example/maparoomview/data/BaseDeDatos.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Lugar::class, TipoLugar::class], version = 6)
+@Database(entities = [Lugar::class, TipoLugar::class], version = 7)
 abstract class BaseDeDatos : RoomDatabase() {
 
     abstract fun LugarDao(): LugarDao
